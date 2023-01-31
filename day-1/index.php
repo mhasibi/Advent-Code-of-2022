30634524850f4577ed0c12dbadd5c98699f3e2d6
Mojan Hasibi <mhasibi@fortinet.com>
2023-01-30 23:06:46 -0800

    day 1 - part 1

---
+++ day-1/index.php
@@ -0,0 +1,20 @@
+<?php
+$filename = 'day-1.txt';
+$numbers = file($filename);
+
+$max_calories = 0;
+$subtotal = 0;
+
+foreach ($numbers as $number) {
+    if (trim($number) != '') {
+        $subtotal += (int) $number;
+    } else {
+        if ($subtotal > $max_calories) {
+            $max_calories = $subtotal;
+        }
+        $subtotal = 0;
+    }
+}
+
+echo $max_calories;
+?>
